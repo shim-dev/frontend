d4ca4caa3599acbed027ac1e6dc72ad37a4bbcf0
sua00 <suasua0105@gmail.com>
2025-05-27 15:16:41 +0900

    feat: 마이페이지 메인 화면에서 뒤로가기 제거

--- lib/mypage.dart
+++ lib/mypage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:capstone_trial_01/appbar.dart'; 
 import 'package:capstone_trial_01/fix_myProfile.dart'; 
 
 class MyTab extends StatelessWidget {
@@ -9,7 +8,7 @@ class MyTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(title: '마이페이지'),
+      appBar: const MypageAppBar(title: '마이페이지'),
       backgroundColor: Colors.white,
       body: ListView(
         children: const [
@@ -20,6 +19,51 @@ class MyTab extends StatelessWidget {
       ),
     );
   }
+}
+
+class MypageAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final VoidCallback? onBack;
+  final Widget? action;
+
+  const MypageAppBar({
+    super.key,
+    required this.title,
+    this.onBack,
+    this.action,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: onBack == null
+          ? null // 뒤로가기 버튼을 제거
+          : IconButton(
+              icon: SvgPicture.asset(
+                'assets/icon/arrow_back.svg',
+                width: 32,
+                height: 32,
+              ),
+              onPressed: onBack ?? () => Navigator.pop(context),
+            ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      elevation: 0,
+      actions: action != null ? [action!] : null,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
 
 class MyProfile extends StatelessWidget {
