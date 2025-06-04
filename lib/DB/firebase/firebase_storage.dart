1595b0c2fb182798f693aa8ca3a388e8b2e55707
chaelim <dlcofla414@naver.com>
2025-06-04 23:48:49 +0900

    feat: 파이어베이스 연동

---
+++ lib/DB/firebase/firebase_storage.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+
+Future<String?> uploadProfileImageToFirebase(File file, String userId) async {
+  try {
+    final storageRef = FirebaseStorage.instance.ref().child('profile_images/$userId.jpg');
+    final metadata = SettableMetadata(contentType: 'image/jpeg');
+
+    final uploadTask = await storageRef.putFile(file, metadata);
+    final downloadUrl = await uploadTask.ref.getDownloadURL();
+
+    return downloadUrl;
+  } catch (e) {
+    print(' Firebase Storage 업로드 실패: $e');
+    return null;
+  }
+}
